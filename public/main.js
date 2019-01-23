3b37065b6b72a7660e170aaf9c7b438acb5d5fde
sartoriusrex <dennis.q.mai@gmail.com>
2019-01-22 22:26:01 -1000

    add first display logic for news in slideshow; pass in news articles to slideshow; style slideshow list items

--- public/main.js
+++ public/main.js
@@ -1,15 +1,29 @@
-// Change Layout of News Article Container depending on if the image is wider than it is tall.
 
-var newsLinkContainer = document.getElementsByClassName("newsLink");
-var newsPhoto = document.getElementsByClassName("newsPhotoRow");
+  // Change Layout of News Article Container depending on if the image is wider than it is tall.
 
-Array.from(newsLinkContainer).forEach( ( newslinkcontainer ) => {
-  Array.from(newsPhoto).forEach( ( newsphoto ) => {
-    if ( newsphoto.naturalHeight < newsphoto.naturalWidth ) {
-      newslinkcontainer.classList.add("newsLinkColumn");
-      newslinkcontainer.classList.remove("newsLinkRow");
-      newsphoto.classList.add("newsPhotoColumn");
-      newsphoto.classList.remove("newsPhotoRow");
-    }
+
+  var newsLinkContainer = document.getElementsByClassName("newsLink");
+  var newsPhoto = document.getElementsByClassName("newsPhotoRow");
+
+
+  Array.from(newsLinkContainer).forEach( ( newslinkcontainer ) => {
+    Array.from(newsPhoto).forEach( ( newsphoto ) => {
+      if ( newsphoto.naturalHeight < newsphoto.naturalWidth ) {
+        newslinkcontainer.classList.add("newsLinkColumn");
+        newslinkcontainer.classList.remove("newsLinkRow");
+        newsphoto.classList.add("newsPhotoColumn");
+        newsphoto.classList.remove("newsPhotoRow");
+      }
+    });
   });
+
+
+// Slideshow display logic;
+
+var sliderListItems = document.getElementsByClassName("sliderLI");
+
+Array.from(sliderListItems).forEach( (sliderli, i) => {
+  if (i > 0) {
+    sliderli.style.display = "none";
+  }
 });
