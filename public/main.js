352d9818576a7647c27c8318303c051826c94a97
sartoriusrex <dennis.q.mai@gmail.com>
2019-01-23 12:02:49 -1000

    restyle background image on news index; show and animate news articles on home index; restyle button hovers to translateY for better effect

--- public/main.js
+++ public/main.js
@@ -2,12 +2,11 @@
   // Change Layout of News Article Container depending on if the image is wider than it is tall.
 
 
-  var newsLinkContainer = document.getElementsByClassName("newsLink");
-  var newsPhoto = document.getElementsByClassName("newsPhotoRow");
+  var newsLinkContainers = document.getElementsByClassName("newsLinkRow");
+  var newsPhotos = document.getElementsByClassName("newsPhotoRow");
 
-
-  Array.from(newsLinkContainer).forEach( ( newslinkcontainer ) => {
-    Array.from(newsPhoto).forEach( ( newsphoto ) => {
+  Array.from(newsLinkContainers).forEach( ( newslinkcontainer ) => {
+    Array.from(newsPhotos).forEach( ( newsphoto ) => {
       if ( newsphoto.naturalHeight < newsphoto.naturalWidth ) {
         newslinkcontainer.classList.add("newsLinkColumn");
         newslinkcontainer.classList.remove("newsLinkRow");
@@ -19,11 +18,29 @@
 
 
 // Slideshow display logic;
+var sliderIndex = 0;
+slideShow();
 
-var sliderListItems = document.getElementsByClassName("sliderLI");
+function slideShow() {
+  var sliderListItems = document.getElementsByClassName("sliderLI");
+  var articleIndicators = document.getElementsByClassName("square");
 
-Array.from(sliderListItems).forEach( (sliderli, i) => {
-  if (i > 0) {
-    sliderli.style.display = "none";
+  var sliderListArray = Array.from(sliderListItems);
+  var articleIndArray = Array.from(articleIndicators);
+
+  sliderListArray.forEach( (li) => {
+    li.style.display = "none";
+  });
+  articleIndArray.forEach( (ind) => {
+    ind.classList.remove("active");
+  });
+
+  sliderIndex++;
+  if( sliderIndex > sliderListArray.length) {
+    sliderIndex = 1;
   }
-});
+
+  sliderListArray[sliderIndex-1].style.display = "flex";
+  articleIndArray[sliderIndex-1].classList.add("active");
+  setTimeout(slideShow, 4000);
+}
