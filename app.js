ccfe7940e894d0985b38092afc49541e78286633
sartoriusrex <dennis.q.mai@gmail.com>
2019-01-28 16:35:34 -1000

    restyle and structure login and register; begin to add passport

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express         = require("express"),
       app             = express(),
       bodyParser      = require("body-parser"),
       methodOverride  = require("method-override"),
+      passport        = require('passport'),
+      LocalStrategy   = require('passport-local'),
+      flash           = require('connect-flash'),
       mongoose        = require("mongoose"),
       Schema          = mongoose.Schema,
       Report          = require('./models/reports'),
@@ -87,8 +90,31 @@ db.once('open', function() {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 app.set("view engine","ejs");
 app.locals.moment = require('moment');
+
+// ======================================
+
+// PASSPORT CONFIGURATION
+
+app.use(require("express-session")({
+  secret: "The truth is not the truth!",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// app.use(function(req,res, next){
+//  res.locals.currentUser = req.user;
+//  res.locals.error = req.flash("error");
+//  res.locals.success = req.flash("success");
+//  next();
+// });
 
 
 // =======================================
