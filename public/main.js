bb09009235a3d629b4dfa51898bc47f6b26864f8
sartoriusrex <dennis.q.mai@gmail.com>
2019-01-28 22:16:43 -1000

    correct javascript asset loading; begin to manipulate flash message

--- public/main.js
+++ public/main.js
@@ -1,26 +1,5 @@
-// Change Layout of News Article Container depending on if the image is wider than it is tall.
-
-
-
-
-// -----add Click Events to dots---------
-
-
-  var dots = Array.from(document.getElementsByClassName("square"));
-  
-function addSlideNavDot(){
-  dots.forEach(function(dot, i) {
-  dot.addEventListener("click", function(){
-    currentSlide(i+1);
-  });
-});
-}
-
-addSlideNavDot();
-
 // Flash Message Display Logic
 
-
+var message = Array.from(document.getElementsByTagName('h5'));
 var flashmessage = document.getElementById('flashContainer');
 
-console.log("hello");
