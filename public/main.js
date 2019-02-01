c49c43a86021afd212cec6ac24eacfa9f31f1a2c
sartoriusrex <dennis.q.mai@gmail.com>
2019-01-31 17:26:13 -1000

    position hidden menu over body; display and hide icons with javascript and css classes; add click event to open and close login menu buttons

--- public/main.js
+++ public/main.js
@@ -7,3 +7,24 @@ if (message[0]) {
   flashContainer.style.display = "flex";
 }
 
+// ===Responsive Menu Displays======
+
+// login menu
+
+var openLoginIcon = document.getElementById("openLogin");
+var closeLoginIcon = document.getElementById("closeLogin");
+
+openLoginIcon.addEventListener("click", function (){
+  makeResponsive();
+});
+closeLoginIcon.addEventListener("click", function(){
+  makeResponsive();
+});
+
+function makeResponsive() {
+  var userLoginDiv = document.getElementsByClassName("userLoginDiv");
+  var headerUtil = document.getElementsByClassName("headerUtilities");
+
+  userLoginDiv[0].classList.toggle("responsive");
+  headerUtil[0].classList.toggle("responsive");
+}
