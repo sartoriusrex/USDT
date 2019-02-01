7539482031a6edf4e472659c498aaf9c8ce53e04
sartoriusrex <dennis.q.mai@gmail.com>
2019-02-01 12:48:43 -1000

    add javascript toggle class to display nav menus; begin footer menu responsiveness

--- public/main.js
+++ public/main.js
@@ -59,3 +59,22 @@ function toggleSocialResponsiveness () {
   socialMediaDiv[0].classList.toggle("responsiveSocial");
   headerUtil[0].classList.toggle("responsiveSocial");
 }
+
+// ===HAMBURGER MENU AND SUBMENUS===
+
+var closeMenu = document.getElementById("closeMenu");
+var openMenu = document.getElementById("navIcon");
+
+closeMenu.addEventListener("click", function (){
+  toggleNavMenu();
+});
+
+openMenu.addEventListener("click", function (){
+  toggleNavMenu();
+});
+
+function toggleNavMenu (){
+  var navlist = document.getElementsByClassName("navlist");
+  navlist[0].classList.toggle("responsive");
+}
+
