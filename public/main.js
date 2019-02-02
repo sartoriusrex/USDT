49935d69848e95fd76d91db9b4ea109db21caa1e
sartoriusrex <dennis.q.mai@gmail.com>
2019-02-02 11:35:13 -1000

    move all javascript in index to main.js and run page specific javascript using document.url; begin changing layout for news index: css finished: just need to properly select url and run script

--- public/main.js
+++ public/main.js
@@ -138,3 +138,134 @@ function toggleFooterIndicies() {
   
   footerIndicies[0].classList.toggle("responsive");
 }
+
+
+
+
+// ===News Show Change Layout based on Image dimensions
+// Change Layout of News Article Container depending on if the image is wider than it is tall.
+
+
+var pageURL = window.document.URL;
+var newsString = "news/";
+
+if (pageURL.includes(newsString)) {
+  changeNewsShowLayout();
+}
+
+
+function changeNewsShowLayout () {
+
+}
+
+
+// ===HOME PAGE LAYOUT================================
+// if Home, make slideshow happen and change layout of news articles
+var homeString = "\\/";
+
+// slidexIndex var
+var slideIndex = 1;
+
+if (pageURL.indexOf(homeString) === -1) {
+  changeNewsLayout();
+  addSlideNavDot();
+  mountComponent();
+  addSlideNav();
+}
+
+// ===NewsLayout based on image size===
+
+function changeNewsLayout() {
+  var newsLinkContainers = document.getElementsByClassName("newsLinkRow");
+  var newsPhotos = document.getElementsByClassName("newsPhotoRow");
+  Array.from(newsLinkContainers).forEach( ( newslinkcontainer ) => {
+    Array.from(newsPhotos).forEach( ( newsphoto ) => {
+      if ( newsphoto.naturalHeight < newsphoto.naturalWidth ) {
+        newslinkcontainer.classList.add("newsLinkColumn");
+        newslinkcontainer.classList.remove("newsLinkRow");
+        newsphoto.classList.add("newsPhotoColumn");
+        newsphoto.classList.remove("newsPhotoRow");
+      }
+    });
+  });
+}
+
+// ======Slideshow Logic======
+
+
+
+// ----------begin slideshow----------------
+
+function mountComponent() {
+  this.showSlides(slideIndex);
+  myTimer = setInterval(() => {this.plusSlides(1);}, 3500);
+}
+
+// ----------Remove visibility and add visibility based on index-------------
+
+function showSlides( n ) {
+  var i;
+  var slides = Array.from(document.getElementsByClassName("sliderLI"));
+  var dots = Array.from(document.getElementsByClassName("square"));
+
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].classList.add("active");
+}
+
+// -------Next and Previous Slide Logic-----------
+
+function plusSlides( n ) {
+  clearInterval(myTimer);
+  this.showSlides(slideIndex += n);
+  if ( n = -1 ) {
+    myTimer = setInterval(() => {this.plusSlides(n+2);}, 3500);
+  } else {
+    myTimer = setInterval(() => {this.plusSlides(n+1);}, 3500);
+  }
+}
+
+function currentSlide( n ) {
+  clearInterval(myTimer);
+  this.showSlides( slideIndex = n );
+  if ( n = -1 ) {
+    myTimer = setInterval(() => {this.plusSlides(n+2);}, 3500);
+  } else {
+    myTimer = setInterval(() => {this.plusSlides(n+1);}, 3500);
+  }
+}
+
+// Add slideshow controls to buttons
+
+function addSlideNav(){
+  var leftNav = Array.from(document.getElementsByClassName("slideleft"));
+  var rightNav = Array.from(document.getElementsByClassName("slideright"));
+
+  leftNav[0].addEventListener("click",function (){
+    plusSlides(-1);
+    });
+  rightNav[0].addEventListener("click",function() {
+    plusSlides(1);
+    });
+}
+
+// -----add Click Events to dots---------
+
+function addSlideNavDot(){
+  var dots = Array.from(document.getElementsByClassName("square"));
+
+  dots.forEach(function(dot, i) {
+  dot.addEventListener("click", function(){
+    currentSlide(i+1);
+    });
+  });
+}
+
