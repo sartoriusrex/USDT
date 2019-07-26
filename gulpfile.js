1003a50b5104610c42c2298cda4fcd93419b1421
sartoriusrex <dennis.q.mai@gmail.com>
2019-07-25 21:28:42 -1000

    bring in sass, split main scss file and import modules to it, including variables, compile it using just sass for now and will bring in gulp after modifications with scss; optimize images; refactor navbar

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,185 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+// const { watch, series, src, dest } = require('gulp');
+// const autoprefixer = require('gulp-autoprefixer');
+// const browserSync = require('browser-sync').create();
+// const reload = browserSync.reload;
+// const sass = require('gulp-sass');
+// const cleanCSS = require('gulp-clean-css');
+// const sourcemaps = require('gulp-sourcemaps');
+// const concat = require('gulp-concat');
+// const imagemin = require('gulp-imagemin');
+// const changed = require('gulp-changed');
+// const uglify = require('gulp-uglify');
+// const nodemon = require('gulp-nodemon');
 
-exports.default = defaultTask
+// // Steps
+// // Concat all scss files to main scss file
+// // compile scss files to css file
+// // minify css file
+// // clean up other files
+// // repeat for javascript
+// // minify media
+// // start up nodemon
+// // watch for changes to scss files and javascript files
+
+// function concatSass() {
+//   return src([ 'src/scss/*.scss' ])
+//   .pipe( sourcemaps.init({loadMaps: true, largeFile: true}))
+//   .pipe( concat( 'style.min.scss' ) )
+//   .pipe( cleanCSS() ) 
+//   .pipe( sourcemaps.write( './maps/' ) )
+//   .pipe( gulp.dest( 'public/css' ) );
+// }
+
+// function compileSass() {
+//   return src([ 'src/scss/*.scss' ])
+//   .pipe( sass() )
+//   .pipe( dest( 'src/css' ));
+// }
+
+// function minifyCss() {
+//   return src( 'src/css/main.css')
+// }
+
+// function initNodemon(){
+//   nodemon({
+//     script: 'app.js',
+//     ext: 'js html ejs scss', 
+//   });
+
+// }
+
+// function watch( done ) {
+//   watch( 'src/scss/*.scss', series( compileSass, initNodemon ));
+//   watch( 'views/**/*.ejs', initNodemon );
+
+//   done()
+// }
+
+// function serve() {
+//   compileSass();
+//   initNodemon();
+//   watch();
+// }
+
+// exports.default = series( serve )
+
+// // const scss = './src/scss/';
+// // const scssDest = './src';
+
+// // const jsSrc = './src/js/';
+// // const jsDest = './public/js';
+
+// // const cssSrc = './src/scss/';
+// // const cssDest = './public/css';
+
+// // const jsWatch = '**/*.js';
+// // const cssWatch = '**/*.scss';
+
+// // const jsFiles = [
+// //   jsSrc + 'main.js'
+// // ]
+
+// // const cssFiles = [
+// //   cssSrc + 'main.css'
+// // ]
+
+// // const imgSrc = './src/imgs';
+// // const imgDest = './public/photos';
+
+// // function compileCss() {
+// //   return gulp.src( [ 'src/scss/*.scss' ] )
+// //   .pipe( sourcemaps.init({ loadMaps: true }))
+// //   .pipe( sass({
+// //     outputStyle: 'expanded'
+// //     }).on( 'error', sass.logError))
+// //   .pipe( autoprefixer( 'last 2 versions' ) )
+// //   .pipe( sourcemaps.write() )
+// //   .pipe( gulp.dest( 'public/css' ) );
+// // }
+
+// // function concatCss() {
+// //   return gulp.src( ['public/css/*.css'] )
+// //   .pipe( sourcemaps.init({loadMaps: true, largeFile: true}))
+// //   .pipe( concat( 'style.min.css' ) )
+// //   .pipe( cleanCSS() ) 
+// //   .pipe( sourcemaps.write( './maps/' ) )
+// //   .pipe( gulp.dest( 'public/css' ) );
+// // }
+
+// // function javascript() {
+// //   return gulp.src( jsFiles )
+// //   .pipe( concat( 'javascript.min.js' ) )
+// //   .pipe( uglify() )
+// //   .pipe( gulp.dest( jsDest ) );
+// // }
+
+// // function imgmin() {
+// //   return gulp.src( imgSrc )
+// //   .pipe(changed( imgDest))
+// //       .pipe( imagemin([
+// //         imagemin.gifsicle({interlaced: true}),
+// //         imagemin.jpegtran({progressive: true}),
+// //         imagemin.optipng({optimizationLevel: 5})
+// //       ]))
+// //       .pipe( gulp.dest( imgDest ) );
+// // }
+
+// // function build() {
+// //   return gulp.series( compileCss, concat, javascript );
+// // }
+
+// // function initNodemon( done ) {
+// //   const STARTUP_TIMEOUT = 5000;
+// //   const server = nodemon({
+// //     script: 'app.js',
+// //     stdout: false // without this line the stdout event won't fire
+// //   });
+// //   let starting = false;
+
+// //   const onReady = () => {
+// //     starting = false;
+// //     done();
+// //   };
+
+// //   server.on('start', () => {
+// //     starting = true;
+// //     setTimeout( onReady, STARTUP_TIMEOUT );
+// //   });
+
+// //   server.on( 'stdout', ( stdout ) => {
+// //     process.stdout.write( stdout ); // pass the stdout through
+// //     if ( starting ) {
+// //       onReady();
+// //     }
+// //   });
+// // }
+
+// // function initBrowserSync( done ) {
+// //   browserSync.init({
+// //     open: 'external',
+// //     proxy: 'http://localhost:3000',
+// //     port: 3000,
+// //     browser: "google chrome",
+// //   }, done );
+// // }
+
+// // function watch() {
+// //   gulp.watch( cssWatch, gulp.series( compileCss, concatCss, javascript ));
+// //   gulp.watch( jsFiles, javascript );
+// //   gulp.watch( imgSrc, imgmin );
+// //   gulp.watch([ 
+// //     jsWatch, 
+// //     jsDest + 'javascript.min.js', 
+// //     cssDest + 'style.min.css'])
+// //     .on( 'change', reload );
+// // }
+
+// // exports.initBrowserSync = initBrowserSync;
+// // exports.initNodemon = initNodemon;
+// // exports.compileCss = compileCss;
+// // exports.concatCss = concatCss;
+// // exports.javascript = javascript;
+// // exports.watch = watch;
+// // exports.imgmin = imgmin;
+
+// // var initialize = gulp.series( initNodemon, initBrowserSync, watch );
--- readme.md
+++ readme.md
@@ -3,23 +3,23 @@ This is a Satire Website, available for fun at https://usdt-satire.herokuapp.com
 This is a personal project website for a ficticious US Government Agency called The US Department of Truth. It is a work of satire designed to amuse the audience while practicing my personal skills in web development.
 
 Technologies used:
-1. HTML, CSS, Javascript
-2. node.js, express, ejs, Mongodb, mongoose
+1. HTML, CSS, Vanilla Javascript
+2. node, express, ejs, Mongodb, mongoose
 3. Passport, moment.js, Heroku, mlab, Inkscape(svg)
+4. Scss, gulp
 
 
 Important Note: 
 
-Date: Feb, 4 2019: This is a personal project, for fun and learning. It is still being built, and the majority of the pages are not yet built, nor are many of the major features or specifications.
+Date: July, 23 2019: This is a personal project, for fun and learning. It is still being built, and the majority of the pages are not yet built, nor are many of the major features or specifications.
 
 Features not yet implemented:
-- Recover Password
+- Recover/ Reset Password
 - Login with email
 - Login with oAuth, Facebook or Google
 - email Newsletter
 - Profile or settings pages
 - Any links in nav bar of footer
-- Small Web Store
 - Form Validation (in any forms) - will come soon during next major deploy
 
 Features coming soon:
@@ -27,7 +27,7 @@ Features coming soon:
 - Article Creation
 - Newsletter and password recovery (within several months)
 - Actual Pages and social media posts with "relevant" content
-- SCSS with Grunt
+- SCSS with gulp
 - Form Validation
 - Password Reset and Account Recovery
 - HTML WCAG compliance coming very soon
